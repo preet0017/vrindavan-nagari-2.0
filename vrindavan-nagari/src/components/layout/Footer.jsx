6d2dd626fd1a2468eb880e8ad1f537cb0ab419ba
Yash Valecha <yashvalecha133@gmail.com>
2026-07-15 01:46:37 +0530

    navratri video added, next section demo added

---
+++ vrindavan-nagari/src/components/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import sectionTwo from '../../assets/images/texture.jpg'
+
+const Footer = () => {
+  return (
+    <div className='w-full'>
+        <img src={sectionTwo} alt="texture" />
+    </div>
+  )
+}
+
+export default Footer
--- vrindavan-nagari/src/components/sections/OpeningScene.jsx
+++ vrindavan-nagari/src/components/sections/OpeningScene.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../layout/Navbar";
 import Hero from "./Hero";
+import Footer from "../layout/Footer";
+import About from "./About";
 
 export default function OpeningScene() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Footer /> 
     </>
   );
 }
